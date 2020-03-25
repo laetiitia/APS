350891543f2bb1d3c5047c9fa1c08f3602984233
laetiitia <phm.laetiti@gmail.com>
2020-03-25 13:09:11 +0100

    eval

--- APS0/eval.ml
+++ APS0/eval.ml
@@ -1,8 +1,8 @@
 open Ast
 
 type valeurs = InN of int 
-    | InF of expr * string list * (string * valeurs) list 
-    | InFR of string * valeurs
+    | InF of expr * AstId list * (AstId * valeurs) list 
+    | InFR of AstId * valeurs
 ;;
 
 (* FONCTIONS UTILES *)
@@ -11,6 +11,7 @@ let eval_int v =
   | InN(x) -> x
   | _ -> failwith "not in N"
 ;;
+
 
 let eval_oprim op e1 e2 =
   match op with
@@ -31,6 +32,18 @@ let eval_opUnary op e =
   | _ -> failwith "not an opUnary"
 ;;
 
+let rec eval_args args acc =
+  match args with 
+  | [] -> acc
+  | (id,t)::suite -> eval_args suite id::acc
+;;
+
+let rec eval_exprs exprs env acc =
+  match exprs with 
+  | [] -> acc
+  | e::es -> (eval_expr env e)::(eval_exprs es acc)
+
+
 let rec eval_expr env e =
   match e with
   | AstNum(x) -> InN(x)
@@ -40,8 +53,10 @@ let rec eval_expr env e =
   | AstPrim(op, l) -> eval_oprim (string_of_oprim op) (eval_expr env (List.hd l)) (eval_expr env (List.nth l 1))
   | AstUnary(op, e) -> eval_opUnary (string_of_opUnary op) (eval_expr env e)
   | AstIf(cond, body, alt) -> if (eval_expr env cond) == InN(1) then (eval_expr env body) else (eval_expr env alt)
-  (*| AstAbstaction(args, e) -> 
-  | AstApply(e, list) -> *)
+  | AstAbstaction(args, e) -> InF(e, (eval_args args []), env)
+  | AstApply(e, exprs) -> let (expr, listId, env2) = eval_expr env e in let l = eval_exprs env exprs [] in 
+                            let newG = env2 in List.iter2 (fun x y -> (x,y)::newG) listId l;
+                            eval_expr newG expr
   | _ -> failwith "not an Expression"
 ;;
 
@@ -62,8 +77,8 @@ let rec eval_dec env dec =
 let rec eval_cmds env cmd =
   match cmd with
   | AstStat(x) -> eval_stat env x
-  | AstDec( x , y) -> let newEnv = eval_dec env x in eval_cmds newEnv y
-  | AstStats ( x , y) -> eval_stat env x ; eval_cmds env y 
+  | AstDec(x , y) -> let newEnv = eval_dec env x in eval_cmds newEnv y
+  | AstStats (x , y) -> eval_stat env x ; eval_cmds env y 
   | _ -> failwith "not a commande"
 ;;
 
